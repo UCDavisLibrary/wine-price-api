0132add3285eb23c5cc1e0eb44894a90ee180406
Quinn Hart <qjhart.ucdavis.edu>
2017-06-07 12:55:47 -0700

    Added example import for media.

---
+++ example/csv_to_media.sql
@@ -0,0 +1,84 @@
+create table catalog_pages (
+media_id uuid,
+filename text,
+page integer,
+contents bytea,
+thumbnail_png bytea,
+ocr text);
+
+-- Get the new catalogs
+-- for i in example/csv/*.csv; do dcpg psql -d wine_price -c "\COPY catalog_pages (filename,page,contents,thumbnail_png,ocr) from '/app/$i' with csv header"
+
+
+-- Add media_ids to the pages.
+update catalog_pages set media_id=gen_random_uuid();
+
+-- Fix the ocr input.
+update catalog_pages set ocr=convert_from(ocr::bytea,'utf8')
+where page is null;
+
+-- Note the media_id = catalog_id
+-- Add the catalog to the media table
+insert into media (media_id,title,contents,thumbnail_png,ocr)
+select
+p.media_id,
+replace(regexp_replace(filename,'^\d+-(.*).pdf$','\1'),'+',' ') as title,
+contents,
+thumbnail_png,
+ocr
+from catalog_pages p
+where page
+is null;
+-- add to catalog table
+with y as (
+ select
+ media_id,(regexp_matches(filename,'\d\d\d\d'))[1] as year
+ from catalog_pages
+ where page is null
+)
+insert into catalogs (catalog_id,title,publisher,year,media_id)
+select media_id,
+replace(regexp_replace(filename,'^\d+-(.*).pdf$','\1'),'+',' ') as title,
+'Sherry-Lehmann Inc.',
+year::integer,
+media_id
+from catalog_pages left join
+y using (media_id)
+where page is null;
+
+
+-- Now import each page
+-- o is the page ocr info.
+with o as (
+ select filename,media_id as catalog_id,
+ string_to_array(ocr,E'\x0C') as page_ocr
+ from catalog_pages
+ where page is null
+)
+insert into media (media_id,title,contents,thumbnail_png,ocr)
+select
+p.media_id,
+replace(regexp_replace(filename,'^\d+-(.*).pdf$','\1'),'+',' ')||' (pg. '||page||')' as title,
+contents,
+thumbnail_png,
+page_ocr[page+1] as ocr
+from catalog_pages p join
+o using (filename)
+where page
+is not null;
+
+
+-- insert the pages
+with o as (
+ select filename,media_id as catalog_id
+ from catalog_pages
+ where page is null
+)
+insert into pages (page_id,catalog_id,page,media_id)
+select media_id,
+catalog_id,
+page,
+media_id
+from catalog_pages p join
+o using (filename)
+where page is not null;
