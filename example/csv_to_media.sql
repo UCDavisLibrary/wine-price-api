491dd6dc15a87d10dd74ec86f6a3b400512a2f2e
Quinn Hart <qjhart@ucdavis.edu>
2017-08-04 17:52:12 -0700

    Updated catalog upload to match new schema.

--- example/csv_to_media.sql
+++ example/csv_to_media.sql
@@ -7,8 +7,7 @@ thumbnail_png bytea,
 ocr text);
 
 -- Get the new catalogs
--- for i in example/csv/*.csv; do dcpg psql -d wine_price -c "\COPY catalog_pages (filename,page,contents,thumbnail_png,ocr) from '/app/$i' with csv header"
-
+-- for i in Fc/210-Christmas+Catalog+1969.csv; do dcpg psql -c "\COPY catalog_pages (filename,page,contents,thumbnail_png,ocr) from '/app/$i' with csv header"; done
 
 -- Add media_ids to the pages.
 update catalog_pages set media_id=gen_random_uuid();
@@ -19,16 +18,18 @@ where page is null;
 
 -- Note the media_id = catalog_id
 -- Add the catalog to the media table
-insert into media (media_id,title,contents,thumbnail_png,ocr)
+insert into media (media_id,title,contents,thumbnail_png,thumbnail2x_png,ocr)
 select
 p.media_id,
 replace(regexp_replace(filename,'^\d+-(.*).pdf$','\1'),'+',' ') as title,
 contents,
 thumbnail_png,
+thumbnail_png,
 ocr
 from catalog_pages p
 where page
 is null;
+
 -- add to catalog table
 with y as (
  select
@@ -36,12 +37,11 @@ with y as (
  from catalog_pages
  where page is null
 )
-insert into catalogs (catalog_id,title,publisher,year,media_id)
+insert into catalogs (catalog_id,title,publisher,year)
 select media_id,
 replace(regexp_replace(filename,'^\d+-(.*).pdf$','\1'),'+',' ') as title,
 'Sherry-Lehmann Inc.',
-year::integer,
-media_id
+year::integer
 from catalog_pages left join
 y using (media_id)
 where page is null;
@@ -55,11 +55,12 @@ with o as (
  from catalog_pages
  where page is null
 )
-insert into media (media_id,title,contents,thumbnail_png,ocr)
+insert into media (media_id,title,contents,thumbnail2x_png,thumbnail_png,ocr)
 select
 p.media_id,
 replace(regexp_replace(filename,'^\d+-(.*).pdf$','\1'),'+',' ')||' (pg. '||page||')' as title,
 contents,
+thumbnail_png,
 thumbnail_png,
 page_ocr[page+1] as ocr
 from catalog_pages p join
@@ -74,11 +75,10 @@ with o as (
  from catalog_pages
  where page is null
 )
-insert into pages (page_id,catalog_id,page,media_id)
+insert into pages (page_id,catalog_id,page)
 select media_id,
 catalog_id,
-page,
-media_id
+page
 from catalog_pages p join
 o using (filename)
 where page is not null;
